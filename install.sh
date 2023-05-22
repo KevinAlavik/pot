99ebe23eba7b09a5f42a980287096f8014a23398
Kevin Alavik <kevin@alavik.se>
2023-05-22 20:37:07 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 ‹#!/bin/bash
+# Clone the repo
+git clone https://github.com/kevinalavik/pot
+# Go inside of the repo
+cd pot
 # Build the Rust program
 cargo build --release
 # Move the output target to /usr/local/bin/pot
 sudo mv target/release/pot /usr/local/bin/
 # Remove the target directory
 cargo clean
+# Move out and clean
+cd ..
+sudo rm -rf pot/
