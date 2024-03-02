f16a3b970760a3c36fb29e0f6d35094cb6a6a6f3
puffer <kevin@alavik.se>
2024-03-02 11:54:01 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ sudo mv target/release/pot /usr/local/bin/
 cargo clean
 sudo mkdir /etc/pot/
 sudo touch /etc/pot/sources.json
-sudo echo '[ "https://puffer.is-a.dev/pot/pot.json" ]' > /etc/pot/sources.json
+sudo echo '[ "https://kevinalavik.github.io/pot/pot.json" ]' > /etc/pot/sources.json
